e5e1fb0018b8af78861aee4c69aef4c5cf77c21e
Jan Barášek <janbarasek@gmail.com>
2023-08-01 14:53:56 +0200

    CrawlerResult: Use arrow function.

--- src/entity/CrawledResult.php
+++ src/entity/CrawledResult.php
@@ -99,9 +99,7 @@ final class CrawledResult
 			'followedUrls' => $this->followedUrls,
 			'openedUrls' => $this->openedUrls,
 			'urlReferences' => $this->urlReferences,
-			'urls' => array_map(function(Url $url): array {
-				return $url->toArray();
-			}, $this->urls),
+			'urls' => array_map(static fn (Url $url): array => $url->toArray(), $this->urls),
 			'errors' => $this->errors,
 			'robots' => $this->robots,
 		];
